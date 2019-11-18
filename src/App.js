bcc9b3a92e533a7f7b20593fc09c991e8f803a3e
Zheng Dongwen <1686435878@qq.com>
2019-11-19 01:32:15 +0800

    Initial commit 1

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Todolist from './component/Todolist'
+
+function App() {
+  return (
+      <div>
+          <Todolist/>
+      </div>
+  );
+}
+
+export default App;
